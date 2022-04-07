d545c0f1d6915cbbce8d0ca931d3a9d9394bed28
salahbelhadj <salahb99@gmail.com>
2022-04-07 20:58:19 +0200

    modification: j'ai rajouté un Id a la suite de chaque form en + a la fin des radio location et les checkboxs en JS j'ai rajouté des lignes de code comme dans le DOM toutles locations + des regex sur les inputs plus des consts sur des texterrors, plus j'ai declaré une variable inputValue je lui affecter une valeur input.value et en fin jai verifié ca avec la if et else et jai refais la meme chose avec les radios de location.

--- app.js
+++ app.js
@@ -1,150 +1,190 @@
-// Nav responsive
 function editNav() {
-  let navbar = document.getElementById('top_nav')
-  if (navbar.className === 'top_nav') {
-    navbar.className += ' responsive'
+  let x = document.getElementById("top_nav");
+  if (x.className === "top_nav") {
+    x.className += " responsive";
   } else {
-    navbar.className = 'top_nav'
+    x.className = "top_nav";
   }
 }
 
-// Methode de selecteur de requête
-const modalBg = document.querySelector('.bground'),
-  modalBtn = document.querySelectorAll('.modal_btn'),
-  formData = document.querySelectorAll('.formData'),
-  thankBtn = document.querySelectorAll('.thank_btn'),
-  thankBg = document.querySelector('.bground_thank')
+// DOM Elements (Query selector method)
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const btnClose = document.querySelector("#closebtn");
+const btnValidation = document.getElementById("btn-thanks");
 
-// Clic "je m'inscris"
-modalBtn.forEach((btn) => btn.addEventListener('click', launchModal))
-
-// Lancement du formulaire (changement du display en block)
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// launch modal form
 function launchModal() {
-  modalBg.style.display = 'block'
+  modalbg.style.display = "block";
 }
 
-// fermer du formulaire via la croix
-document.getElementById('closeform').addEventListener('click', () => {
-  modalBg.style.display = 'none'
-})
+// function to remove form
+function close() {
+  modalbg.style.display = "none";
+}
 
-// fermer thanks via "fermer"
-document
-  .querySelector('.button_thank')
-  .addEventListener('click', () => {
-    thankBg.style.display = 'none'
-    modalBg.style.display = 'none'
-  })
+// "X" close button of the form
+btnClose.addEventListener("click", close);
 
-// fermer thanks via croix
-document
-  .getElementById('close_thanks')
-  .addEventListener('click', () => {
-    thankBg.style.display = 'none'
-    modalBg.style.display = 'none'
-  })
+// Inputs Value
+const nameInput = document.getElementById("first"),
+      lastNameInput = document.getElementById("last"),
+      emailInput = document.getElementById("email"),
+      birthdateInput = document.getElementById("birthdate"),
+      quantityInput = document.getElementById("quantity"),
+      location1Input = document.getElementById("location1"),
+      location2Input = document.getElementById("location2"),
+      location3Input = document.getElementById("location3"),
+      location4Input = document.getElementById("location4"),
+      location5Input = document.getElementById("location5"),
+      location6Input = document.getElementById("location6"),
+      conditionsCheckBox1 = document.getElementById("checkbox1")
 
-// Function Message erreur
-function showErrorMessage(champForm, message) {
-  champForm.target.setCustomValidity('')
-  if (!champForm.target.validity.valid && champForm.target.value.length == 0) {
-    champForm.target.setCustomValidity(message)
+// Regex
+const nameRegex = /^[a-z ,.'-][a-z ,.'-]+$/i;
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const quantityRegex = /^[0-9]+/;
+const birthdateRegex =
+  /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+
+// div where to insert error text
+const errorText = [
+  "texterrorfirstname",
+  "texterrorlastname",
+  "texterroremail",
+  "texterrorbirthdate",
+  "texterrorquantity",
+  "texterrorlocation",
+  "texterrorterms",
+];
+
+// all the error texts
+const errorTexts = [
+  "Please enter 2 or more characters for the first name field",
+  "Please enter 2 or more characters for the name field",
+  "Please enter a valid email address",
+  "You must enter your date of birth",
+  "Please indicate your number of games",
+  "You must to choose a city",
+  "You must verify that you accept the terms and conditions",
+];
+
+// function to validate First name, Last name, Email, Birthdate, Quantity (tournament)
+const validInput = (input, regex, errorTextId, errorText) => {
+  let inputValue = input.value;
+  if (regex.test(inputValue)) {
+    document.getElementById(errorTextId).innerText = "";
+    return true;
+  } else {
+    document.getElementById(errorTextId).innerHTML = errorText;
+    return false;
   }
-}
+};
 
-// Messages d'erreurs par champs pour le formulaire
-document.getElementById("inscription")
-.addEventListener("click", (formRempli) => {
-    let firstName = document.getElementById('first')
-    let lastName = document.getElementById('last')
-    let emailValid = document.getElementById('email')
-    let birthValid = document.getElementById('birthdate')
-    let quantityCity = document.getElementById('quantity')
-    let locations = document.getElementById('locations')
-    let closeThanks = document.getElementById('bgThanks')
+// function radio
+const validateRadio = (event) => {
+  let checkedLocation1Input = location1Input.checked;
+  let checkedLocation2Input = location2Input.checked;
+  let checkedLocation3Input = location3Input.checked;
+  let checkedLocation4Input = location4Input.checked;
+  let checkedLocation5Input = location5Input.checked;
+  let checkedLocation6Input = location6Input.checked;
+  if (
+    checkedLocation1Input == false &&
+    checkedLocation2Input == false &&
+    checkedLocation3Input == false &&
+    checkedLocation4Input == false &&
+    checkedLocation5Input == false &&
+    checkedLocation6Input == false
+  ) {
+    document.getElementById(errorText[5]).innerHTML = errorTexts[5];
+    return false;
+  } else {
+    document.getElementById(errorText[5]).innerHTML = "";
+    return true;
+  }
+};
 
-    closeThanks.onclick = (event) => {
-      if (event.target == closeThanks) {
-        modal.style.display = "none"
-      }
-    }
+// function checkbox (termes)
+const validateCheckbox = (event) => {
+  if (!conditionsCheckBox1.checked) {
+    document.getElementById(errorText[6]).innerHTML = errorTexts[6];
+    return false;
+  } else {
+    document.getElementById(errorText[6]).innerHTML = "";
+    return true;
+  }
+};
 
-    if (!firstName.value) {
-      firstName.oninvalid = (champForm) => {
-        showErrorMessage(
-          champForm,
-          'Please enter 2 or more characters for the first name field.'
-        )
-      }
-      return false
-    }
-    if (!lastName.value) {
-      lastName.oninvalid = (champForm) => {
-        showErrorMessage(
-          champForm,
-          'Please enter 2 or more characters for the name field.'
-        )
-      }
-      return false
-    }
-    if (!emailValid.value) {
-      emailValid.oninvalid = (champForm) => {
-        showErrorMessage(
-          champForm,
-          'Enter a valid address. Example: contact@name.com'
-        )
-      }
-      return false
-    }
-    if (!birthValid.value) {
-      birthValid.oninvalid = (champForm) => {
-        showErrorMessage(
-          champForm,
-          'You must enter your date of birth.'
-        )
-      }
-      return false
-    }
-    if (!quantityCity.value) {
-      quantityCity.oninvalid = (champForm) => {
-        showErrorMessage(champForm,
-          'You must fill in this field.')
-      }
-      return false
-    }
+// FONCTION DE VALIDATION
+// First name
+const validate = () => {
+  const isFirstNameValid = validInput(
+    nameInput,
+    nameRegex,
+    errorText[0],
+    errorTexts[0]
+  );
+  // Last name
+  const isLastNameValid = validInput(
+    lastNameInput,
+    nameRegex,
+    errorText[1],
+    errorTexts[1]
+  );
+  // Email
+  const isEmailValid = validInput(
+    emailInput,
+    emailRegex,
+    errorText[2],
+    errorTexts[2]
+  );
+  // Birthdate
+  const isBirthdateValid = validInput(
+    birthdateInput,
+    quantityRegex,
+    errorText[3],
+    errorTexts[3]
+  );
+  // Quantity (tournament)
+  const isQuantityValid = validInput(
+    quantityInput,
+    quantityRegex,
+    errorText[4],
+    errorTexts[4]
+  );
+  // Radio
+  const isRadioValid = validateRadio();
+  // Checkbox (CGU)
+  const isCheckboxValid = validateCheckbox();
 
-    if (quantityCity.value && quantityCity.value > 0) {
-      let numberCitiesChecked = locations.querySelectorAll('input[type="checkbox"]:checked').length
-      if (numberCitiesChecked === 0) {
-        document.getElementById('locationMsg').classList.remove("hidden")
-        return false
-      } else {
-        document.getElementById('locationMsg').classList.add("hidden")
-      }
-    }
+  return (
+    isFirstNameValid &&
+    isLastNameValid &&
+    isEmailValid &&
+    isBirthdateValid &&
+    isQuantityValid &&
+    isRadioValid &&
+    isCheckboxValid
+  );
+};
 
-    if (quantityCity.value) {
-      let numberCitiesChecked = locations.querySelectorAll('input[type="checkbox"]:checked').length
-      if (quantityCity.value < numberCitiesChecked) {
-        document.getElementById('locationMsg2').classList.remove("hidden")
-        return false
-      } else {
-        document.getElementById('locationMsg2').classList.add("hidden")
-      }
-    }
+// form sending
+document
+  .getElementById("inscription")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // on le met si erreur afin de ne pas envoyé le formulaire
 
-    thankBtn.forEach((btn) => btn.addEventListener('click', launchThank))
-    function launchThank() {
-      thankBg.style.display = 'block'
+    if (validate(event)) {
+      const messageValidation = document.getElementById("thanksMessage");
+      messageValidation.style.display = "block";
+      const form = document.getElementById("inscription");
+      form.style.display = "none";
     }
-    formRempli.preventDefault()
   });
 
-    let closeThanks = document.getElementById('bgThanks');
-
-// When the user clicks anywhere outside of the modal, close it
-    window.onclick = (event) => {
-    if (event.target == closeThanks) {
-    closeThanks.style.display = "none";
-    }
-  }
+// close button after validation
+btnValidation.addEventListener("click", close);
